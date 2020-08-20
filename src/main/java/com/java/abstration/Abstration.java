972c066bbb00ac0acf7c57143767d15f943d7e1c
SriLakshmiPan <SriLakshmiPan@gmail.com>
2020-08-20 17:16:08 -0400

    interfaces and generic methods

---
+++ src/main/java/com/java/abstration/Abstration.java
@@ -0,0 +1,39 @@
+package com.java.abstration;
+
+//Abstation,hide internal data  and showing functionality only
+class Triangle {
+	private float base, height;
+
+	public float getBase() {
+		return base;
+	}
+
+	public void setBase(float base) {
+		this.base = base;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public float area() {
+		float result = (0.5f) * base * height;
+		return result;
+	}
+}
+
+public class Abstration {
+	public static void main(String[] args) {
+		Triangle t = new Triangle();
+		t.setBase(5.0f);
+		t.setHeight(3.0f);
+		float area = t.area();
+		System.out.println("Base = " + t.getBase());
+		System.out.println("Height = " + t.getHeight());
+		System.out.println("Area = " + area);
+	}
+}
---
+++ src/main/java/com/lakshmi/polymorphism/compiletime.java
@@ -0,0 +1,21 @@
+package com.lakshmi.polymorphism;
+
+// compiletime polymorphism,early binding/static binding
+// polymorphism is the ability of an object to take on many forms(one in many ways) 
+//compilertime polymorphism,if the polymorphic nature of an entity is decided by the compiler(uses methodoverloading)
+class compiletime {
+	void add(int x,int y)
+	{
+			System.out.println("Result1:"+(x+y));
+		}
+
+	void add(int x,int y,int z)
+	{
+	System.out.println("Result2:"+(x+y+z));
+	}
+
+	public static void main(String[] args) {
+		compiletime p = new compiletime();
+		p.add(3, 4, 5);//depends on parameters compiler will decide which method has to run
+	}
+}
