5ede8e6081e3985ba0513d58d273967c8362cf09
SriLakshmiPan <SriLakshmiPan@gmail.com>
2020-08-20 17:32:54 -0400

    commenting the error's class

--- src/main/java/com/lakshmi/exception/UserdefinedException.java
+++ src/main/java/com/lakshmi/exception/UserdefinedException.java
@@ -4,19 +4,13 @@ import javax.naming.InsufficientResourcesException;
 
 public class UserdefinedException {
 	public static void main(String[] args) {
-		// to create our own exceptions
-		Checkingaccount c = new Checkingaccount();
-		System.out.println("Deposting $500...");
-		c.deposit(500.00);
-		try {
-			System.out.println("withdrawing $100...");
-			c.withdraw(100.00);
-			System.out.println("withdrawing $600...");
-			c.withdraw(100.00);
-		} catch (InsufficientFundsException e) {
-			System.out.println("sorry,but ur short $" + e.getAmount());
-			e.printStackTrace(a);
-		}
-	}
+		/*
+		 * // to create our own exceptions Checkingaccount c = new Checkingaccount();
+		 * System.out.println("Deposting $500..."); c.deposit(500.00); try {
+		 * System.out.println("withdrawing $100..."); c.withdraw(100.00);
+		 * System.out.println("withdrawing $600..."); c.withdraw(100.00); } catch
+		 * (InsufficientFundsException e) { System.out.println("sorry,but ur short $" +
+		 * e.getAmount()); e.printStackTrace(a); }
+		 */}
 
 }
