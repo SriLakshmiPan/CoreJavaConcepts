98772c73e221e8bb26d0b8c838dc4f2069df4151
SriLakshmiPan <SriLakshmiPan@gmail.com>
2020-08-21 14:52:54 -0400

    Generic class

---
+++ src/main/java/com/java/generic/Genericclass.java
@@ -0,0 +1,23 @@
+package com.java.generic;
+
+import javax.swing.Box;
+
+//generic class 
+public class Genericclass<T> {//T is a generic class
+	private T t;
+	public void add(T t) {
+		this.t=t;
+	}
+	public T get() {
+		return t;
+	}
+
+	public static void main(String[] args) {
+		Genericclass<Integer>integerBox = new Genericclass<Integer>();
+		Genericclass<String> stringBox=new Genericclass<String>();
+integerBox.add(new Integer(10));
+stringBox.add(new String("hello world"));
+System.out.printf("Integre values : %d\n\n",integerBox.get());
+System.out.printf("String values : %s\n",stringBox.get());
+	}
+}
